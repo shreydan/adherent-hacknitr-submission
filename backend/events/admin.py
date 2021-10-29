7281e02efd46fccd0b87ae1317a8bcf3536aabb1
Dev-Jain04 <dakshesh.jain14@gmail.com>
2021-10-29 23:52:04 +0530

    setup admin

--- backend/events/admin.py
+++ backend/events/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+# importing models
+from .models import (
+    Area,
+    City,
+    Event,
+)
+
+
+@admin.register(Area)
+class AreaAdmin(admin.ModelAdmin):
+    list_display = ('area_name',)
+
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    list_display = ('city_name',)
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    '''Admin View for Event'''
+
+    list_display = ('username', 'title',)
+    list_display_links = ('title',)
+    list_per_page = 15
+    search_fields = ('area__area_name', 'city__city_name',)
