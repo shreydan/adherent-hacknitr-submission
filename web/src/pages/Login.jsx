200a6c6fd7f1b9fd73bba1ee3aa1c70612d4e57f
shreydan <iamshreydan@gmail.com>
2021-10-30 18:20:58 +0530

    basic changes

--- web/src/pages/Login.jsx
+++ web/src/pages/Login.jsx
@@ -3,6 +3,7 @@ import '../css/styles.css'
 
 export default function Login() {
   return (
+<<<<<<< HEAD
     <div className="login_container fluid-container">
       
         <h2 class="welcome_header mb-5">Welcome to Adherent</h2>
@@ -23,4 +24,25 @@ export default function Login() {
       
     </div>
   );
+=======
+    
+    <div className="container">
+     <div className="w-100"> 
+     <div class="card mx-auto d-flex align-items-center" style={{width: "25rem" , height:"22rem"}}>
+  <div class="card-body-1 w-100" style={{backgroundColor:"#0d6efd", marginBottom:"2rem" , color:"white"}}>
+    <h2 class="card-title">Join</h2>
+    <h2 class="card-title">Now</h2>
+  </div>
+  <div class="card-body-2">
+    <div class="input-group flex-nowrap">
+  <span class="input-group-text" id="addon-wrapping">@</span>
+  <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="addon-wrapping" style={{width:"20rem"}}/>
+</div> 
+    <a href="/" class="btn btn-primary"  style={{width:"23rem" , marginTop:"2rem"}}>Join</a>
+</div>
+</div>
+       </div>     
+    </div>  
+    )
+>>>>>>> 6f1c6771dc04cfa5f2e5c779dce8654f7a0bd47e
 }
