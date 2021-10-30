9aa6cadc2b476c6084183b6a63028fb63b754a3e
Dev-Jain04 <dakshesh.jain14@gmail.com>
2021-10-30 12:19:29 +0530

    changed pagination to 10

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -155,7 +155,7 @@ CORS_ALLOW_ALL_ORIGINS = True  # other defaults are good for development
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 15,
+    'PAGE_SIZE': 10,
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',  # just for development
